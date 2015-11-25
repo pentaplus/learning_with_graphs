b124f32b8dcbe758e3e8fca098540f6e8895b6c3
Benjamin Plock <benjamin.plock@stud.uni-goettingen.de>
2015-11-25 16:48:30 +0100

    exp_feature_maps changed

--- code/exp_feature_maps/exp_feature_maps.py
+++ code/exp_feature_maps/exp_feature_maps.py
@@ -2,7 +2,7 @@ import networkx as nx
 import os
 import pz
 
-# test section ---------------------------------------------------------------
+# test section -------------------------------------------------------------------
 class MyClass:
     "A simple example class"
     i = 12345
@@ -19,8 +19,6 @@ x.i
 x.i = 0
 x.i
 x.f()
-
-x.in
 
 
 
@@ -45,7 +43,7 @@ a
 b = Complex(7,7)
 b.i
 
-# ----------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 android_fcg_path = os.path.join("..", "..", "datasets", ("ANDROID FCG (2 "
                                 "classes, 26 directed graphs, "
