e93a683e2bd778f25537d1286bb3d227d5fa78fa
pentaplus <learning.with.graphs@gmail.com>
2016-03-22 15:15:15 +0100

    replaced graph no. 13670 because it was empty

--- code/misc/dataset_loader.py
+++ code/misc/dataset_loader.py
@@ -23,6 +23,9 @@ from os.path import abspath, dirname, join
 # determine script path
 SCRIPT_PATH = inspect.getframeinfo(inspect.currentframe()).filename
 SCRIPT_FOLDER_PATH = dirname(abspath(SCRIPT_PATH))
+# modify the search path for modules in order to access modules in subfolders
+# of the script's parent directory
+sys.path.append(join(SCRIPT_FOLDER_PATH, '..'))
 
 from misc import utils
 
@@ -113,9 +116,21 @@ def get_graphs_of_class_dict(graph_meta_data_of_num):
 if __name__ == '__main__':
     DATASET_PATH = join(SCRIPT_FOLDER_PATH, '..', '..', 'datasets')
 #    dataset = 'MUTAG'
-    dataset = 'PTC(MR)'
+#    dataset = 'PTC(MR)'
+    dataset = 'ANDROID FCG 14795'
     
     graph_meta_data_of_num, class_lbls \
         = get_graph_meta_data_and_class_lbls(dataset, DATASET_PATH)
                                                              
     G = pz.load(graph_meta_data_of_num[0][0])
+    
+    
+#G = pz.load(graph_meta_data_of_num[13669][0])
+#G.number_of_nodes()
+#
+#G = pz.load(graph_meta_data_of_num[13670][0])
+#G.number_of_nodes()
+#
+#pz.load(graph_meta_data_of_num[14182][0]).number_of_nodes()
+#
+#G = pz.load('121563fa0518ed7bf843f08958ea1cab302a304dd990ff792bba893ebf716fdf.fcgnx.pckl')
